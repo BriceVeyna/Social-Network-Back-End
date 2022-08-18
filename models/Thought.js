98335d66cd12f1745455586efde7e9ab2ac79660
Brice Veyna <bricev99@gmail.com>
2022-08-18 15:54:44 -0700

    Add reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -22,7 +22,7 @@ const thoughtSchema = new Schema(
         },
         reactions: [{
             type: Schema.Types.ObjectId,
-            ref: 'reaction',
+            ref: reactionSchema,
         }],
     },
     {
@@ -33,12 +33,44 @@ const thoughtSchema = new Schema(
     }
 );
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
 thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
 
 thoughtSchema.virtual('creationTime').get(function () {
+    new Date(creationTime).toLocaleString();
+})
 
+reactionSchema.virtual('creationTime').get(function () {
+    new Date(creationTime).toLocaleString();
 })
 
 const Thought = model('Thought', thoughtSchema);
